07b4934a611d1341e923129913f7156172b5dc3e
David C. Larsen <dcl9934@cs.rit.edu>
2012-04-07 17:50:18 -0400

    All modules now build individually.
    The modules (each subdirectory) build all of the object files and
    produce an archive file for that module.
    
    What's still missing from the build system:
    - Dependency resolution
    	Currently, you're only guaranteed to build correctly on `make -B'
    	builds. We'll have to re-incorporate `make depends' into each
    	module.
    
    - Linking
    	The project doesn't link into one executable yet.

---
+++ src/defs.mk
@@ -0,0 +1,73 @@
+#
+# User compilation/assembly definable options
+#
+#	ISR_DEBUGGING_CODE	include context restore debugging code
+#	CLEAR_BSS_SEGMENT	include code to clear all BSS space
+#	SP2_CONFIG		enable SP2-specific startup variations
+#	REPORT_MYSTERY_INTS	print a message on interrupt 0x27
+#
+USER_OPTIONS = -DCLEAR_BSS_SEGMENT -DSP2_CONFIG -DISR_DEBUGGING_CODE
+
+#
+# YOU SHOULD NOT NEED TO CHANGE ANYTHING BELOW THIS POINT!!!
+#
+# Compilation/assembly control
+#
+
+#
+# We only want to include from the current directory and ~wrc/include
+#
+INCLUDES = -I.
+
+#
+# Compilation/assembly/linking commands and options
+#
+CPP = cpp
+# CPPFLAGS = $(USER_OPTIONS) -nostdinc -I- $(INCLUDES)
+CPPFLAGS = $(USER_OPTIONS) -nostdinc $(INCLUDES)
+
+CC = gcc
+CFLAGS = -fno-stack-protector -fno-builtin -Wall -Wextra -Wstrict-prototypes -m32 $(CPPFLAGS)
+
+AS = as
+ASFLAGS = --32 -n32 
+
+LD = ld
+LDFLAGS = -m elf_i386
+
+LN := ln -f
+
+AR := ar -cr
+
+#		
+# Transformation rules - these ensure that all compilation
+# flags that are necessary are specified
+#
+# Note use of 'cpp' to convert .S files to temporary .s files: this allows
+# use of #include/#define/#ifdef statements. However, the line numbers of
+# error messages reflect the .s file rather than the original .S file. 
+# (If the .s file already exists before a .S file is assembled, then
+# the temporary .s file is not deleted.  This is useful for figuring
+# out the line numbers of error messages, but take care not to accidentally
+# start fixing things by editing the .s file.)
+#
+
+.SUFFIXES:	.S .b
+
+.c.s:
+	$(CC) $(CFLAGS) -S $*.c
+
+.S.s:
+	$(CPP) $(CPPFLAGS) -o $*.s $*.S
+
+.S.o:
+	$(CPP) $(CPPFLAGS) -o $*.s $*.S
+	$(AS) $(ASFLAGS) -o $*.o $*.s -a=$*.lst
+	$(RM) -f $*.s
+
+.s.b:
+	$(AS) $(ASFLAGS) -o $*.o $*.s -a=$*.lst
+	$(LD) $(LDFLAGS) -Ttext 0x0 -s --oformat binary -e begtext -o $*.b $*.o
+
+.c.o:
+	$(CC) $(CFLAGS) -c $*.c
