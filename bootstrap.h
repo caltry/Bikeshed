4ffdabaecdadd922656fd17ef4f70cb740152435
Sean Congden <sean.congden@gmail.com>
2012-04-10 01:28:14 -0400

    WIP on routines to drop down to real mode
    Correct linking of the 16-bit bios.S sections still needed

--- bootstrap.h
+++ bootstrap.h
@@ -27,10 +27,14 @@
 #define	GDT_ADDRESS	0x00000500
 
 	/* segment register values */
-#define	GDT_LINEAR	0x0008		/* All of memory, R/W */
-#define	GDT_CODE	0x0010		/* All of memory, R/E */
-#define	GDT_DATA	0x0018		/* All of memory, R/W */
-#define	GDT_STACK	0x0020		/* All of memory, R/W */
+#define	GDT_LINEAR		0x0008		/* All of memory, R/W */
+#define	GDT_CODE		0x0010		/* All of memory, R/E */
+#define	GDT_DATA		0x0018		/* All of memory, R/W */
+#define	GDT_STACK		0x0020		/* All of memory, R/W */
+#define GDT_CODE_16		0x0028
+#define GDT_DATA_16		0x0030
+#define GDT_STACK_16	0x0032
+
 
 /*
 ** The Interrupt Descriptor Table (0000:2500 - 0000:2D00)
@@ -43,10 +47,10 @@
 */
 #define	MMAP_SEGMENT	0x000002D0
 #define	MMAP_ADDRESS	0x00002D00
-#define	MMAP_EXT_LO	0x00	/* extended memory - low register */
-#define	MMAP_EXT_HI	0x02	/* extended memory - high register */
-#define	MMAP_CFG_LO	0x04	/* configured memory - low register */
-#define	MMAP_CFG_HI	0x06	/* configured memory - high register */
+#define	MMAP_EXT_LO		0x00	/* extended memory - low register */
+#define	MMAP_EXT_HI		0x02	/* extended memory - high register */
+#define	MMAP_CFG_LO		0x04	/* configured memory - low register */
+#define	MMAP_CFG_HI		0x06	/* configured memory - high register */
 #define	MMAP_PROGRAMS	0x08	/* # of programs loaded from disk (+ kernel) */
 #define	MMAP_SECTORS	0x0a	/* table of sector counts for each program */
 
@@ -55,6 +59,9 @@
 */
 
 #define	RMTEXT_SEGMENT	0x00000300
-#define	RMTEXT_ADDRESS	0x00003000
+#define	RMTEXT_ADDRESS	0x00003000	/* and upward */
+#define REAL_STACK		0x00007000	/* and downward */
+#define RM_SAVED_ESP	0x00007004
+#define RM_SAVED_EBP	0x00007008
 
 #endif
