39a2458967762a579f870aaf47fb24a0611074d0
Tietokone <wtl4112@rit.edu>
2012-04-16 17:49:45 -0400

    Updated comments in the paging header

--- src/kernel/memory/paging.h
+++ src/kernel/memory/paging.h
@@ -39,8 +39,43 @@ typedef union PageDirectory
 page_directory_t *__virt_kpage_directory;
 extern page_directory_t boot_page_directory;
 
+/* Initializes paging for our operating system. It identity maps the
+ * first 1MiB of memory meaning the linear and virtual addresses are
+ * the same.
+ *
+ * Next it looks at the kernels linked location (defined in physical.h)
+ * and finds the appropriate place in the kernel page directory for
+ * mapping the kernel pages. The number of pages is KERNEL_SIZE / 4096
+ *
+ * By default with our higher half kernel the page directory looks like
+ * the following
+ *
+ * Key:
+ * R/W = Read/Write
+ * S   = Supervisor
+ * P   = Present
+ *
+ * Index    Page directory     Page Table [index1-index2] == physical address1 - physical address2
+ *        +---------------+
+ *	0     |    R/W S P    | -> [0-256] == 0x0 - 0x100000 R/W S, [256-1023] == 0
+ *        +---------------+
+ * 1-767  |  Not mapped   |
+ *        +---------------+
+ *  768   |    R/W S P    | -> [16-K_END] == 0x100000 - KERNEL_SIZE R/W S, [KERNEL_SIZE-1023] == 0
+ *        +---------------+
+ * 769-   |  Not Mapped   |
+ * 1023	  |               |
+ *        +---------------+
+ */
 void __virt_initialize_paging(void);
 
+/* Changes the current page directory used for virtual memory. This is useful
+ * when context switching so each process can have it's own address space.
+ *
+ * It's really just a wrapper around an assembly call:
+ *
+ *    asm volatile("mov %0, %%cr3" :: "b"(page_directory));
+ */
 void __virt_switch_page_directory(page_directory_t *page_directory);
 
 // Taken from OS Dev wiki
@@ -49,8 +84,21 @@ void* __virt_get_phys_addr(void *virtual_addr);
 // Taken from OS Dev wiki
 void __virt_map_page(void *physical_addr, void *virtual_addr, uint32 flags);
 
+/* Do the reverse of __virt_map_page(). If there is an entry present
+ * it will set it to not present and if all the page table entries are
+ * marked not present it will free the physical address used by the
+ * page table. Finally it invalidates the virtual address in the TLB.
+ *
+ * The address supplied is the virtual address to unmap
+ *
+ * Only 4KiB aligned virtual addresses will have any effect.
+ */
 void __virt_unmap_page(void *virtual_addr);
 
+/* Used to handle page faults. Currently it displays what the
+ * faulting address was and what the error bits were and then
+ * does a kernel panic.
+ */
 void _isr_page_fault(int vector, int code);
 
 /* Defined in memory.S */
