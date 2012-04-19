721c106dae204b5d32c7d6643ef5d46e2c966720
David C. Larsen <dcl9934@cs.rit.edu>
2012-04-19 17:52:50 -0400

    misc: Move public function documentation to the header file.

--- src/fs/ext2/read_ext2.c
+++ src/fs/ext2/read_ext2.c
@@ -29,27 +29,12 @@ get_superblock( Uint32 filesystem_start )
 	sb = filesystem_start + (EXT2_SUPERBLOCK_LOCATION);
 }
 
-/*
- * Dump interesting superblock data from the filesyste, to see if we're reading
- * it properly.
- *
- * Like the output from mke2fs:
- * Filesystem label=
- * OS type: Linux
- * Block size=1024 (log=0)
- * Fragment size=1024 (log=0)
- * Stride=0 blocks, Stripe width=0 blocks
- * 2048 inodes, 8192 blocks
- * 409 blocks (4.99%) reserved for the super user
- * First data block=1
- * Maximum filesystem blocks=8388608
- * 1 block group
- * 8192 blocks per group, 8192 fragments per group
- * 2048 inodes per group
- */
 void print_superblock_data(struct ext2_superblock *sb)
 {
 	serial_string("=== Printing Superblock Data ===\n\r");
+	serial_string( "Volume name: " );
+	serial_string( sb->volume_name );
+	serial_string( "\n\r" );
 	serial_printf("Block size = %d\n\r",
 		(1024 << sb->logarithmic_block_size));
 	serial_printf( "%d inodes, ", sb->inodes_count );
