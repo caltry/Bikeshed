d906e934b6cdb8f5847474d048c2d574fa750ee4
Tietokone <wtl4112@rit.edu>
2012-05-01 16:49:59 -0400

    Added the new, new[], delete, and delete[] operators for C++. Simply calls __kmalloc() and __kfree().

--- src/kernel/cpp/cpptest.cpp
+++ src/kernel/cpp/cpptest.cpp
@@ -8,20 +8,27 @@ extern "C"
 		Object obj(10);
 		const char* output_test = "CPP SUPPORT EXISTS!\n";
 		serial_printf(output_test);
-		serial_printf("Object before: %d\n", obj.get_i());
+		serial_printf("Object 1 before: %d\n", obj.get_i());
 		obj.increment();
-		serial_printf("Object after: %d\n", obj.get_i());
+		serial_printf("Object 1 after: %d\n", obj.get_i());
+
+		serial_printf("Starting new test:\n");
+		Object* obj2 = new Object(20);
+		serial_printf("Object 2 before: %d\n", obj2->get_i());
+		obj2->increment();
+		serial_printf("Object 2 after: %d\n", obj2->get_i());
+		delete obj2;
 	}
 }
 
 Object::Object(int i) : _i(i)
 { 
-	serial_printf("Object constructing!\n");
+	serial_printf("Object %d constructing!\n", i);
 }
 
 Object::~Object()
 {
-	serial_printf("Object destructing!\n");
+	serial_printf("Object %d destructing!\n", _i);
 }
 
 void Object::increment()
