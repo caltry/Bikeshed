2e7ea212dea39f0bc676a508668baf608534f14d
Sean Congden <sean.congden@gmail.com>
2012-05-24 17:40:03 -0400

    Documentation WIP part 32/55

--- src/kernel/video/desktop.h
+++ src/kernel/video/desktop.h
@@ -3,7 +3,7 @@
 **
 ** Author:	Sean Congden
 **
-** Description:	
+** Description:	A graphical desktop that can contain graphical applications.
 */
 
 #ifndef _DESKTOP_H
@@ -24,20 +24,79 @@ class Window;
 class Desktop {
 public:
 
+	/*
+	** Desktop(screen)
+	**
+	** A graphical desktop tied to the provided screen that can
+	** contain graphical applications.
+	*/
 	Desktop(Screen *screen);
 	~Desktop(void);
 
+	/*
+	** AddWindow(window)
+	**
+	** Adds the given window to the desktop.  The window is added
+	** to the top of the window stack and gains focus.
+	*/
 	void AddWindow(Window *window);
+
+	/*
+	** CloseWindow(window)
+	**
+	** Closes and removes the given window from the desktop.
+	*/
 	void CloseWindow(Window *window);
 
+	/*
+	** GetBounds(bounds)
+	**
+	** Provides a rectangle representing the size of the desktop
+	** being rendered.  This is usually the same as the screen resolution.
+	** 
+	** Returns:
+	**      the bounding area of the desktop
+	*/
 	Rect& GetBounds(void) { return bounds; }
+
+	/*
+	** GetPainter()
+	**
+	** Provides a painter linked to the desktop's screen
+	**
+	** Returns:
+	**      a painter for the desktop
+	*/
 	Painter *GetPainter(void) { return painter; }
 
+	/*
+	** HandleMouseEvent(event)
+	**
+	** Updates the desktop cursor based on the mouse event then dispatches
+	** the event to the focused window.
+	*/
 	void HandleMouseEvent(MouseEvent *event);
 
+	/*
+	** Draw()
+	**
+	** Paints the entire desktop including all windows it contains.
+	*/
 	void Draw(void);
+
+	/*
+	** DrawCursor()
+	**
+	** Draws a mouse cursor directly to the screen framebuffer.
+	*/
 	void DrawCursor(void);
 
+	/*
+	** Invalidate()
+	**
+	** Marks the entire desktop as dirty forcing a redraw of the
+	** whole desktop and all of its windows.
+	*/
 	void Invalidate(void) { dirty = true; }
 	
 private:
@@ -53,6 +112,13 @@ private:
 extern "C" {
 #endif
 
+	/*
+	** _desktop_run()
+	**
+	** Starts up VESA based graphics on the system with a basic set
+	** of windows then repaints them in a loop.  This method should
+	** be invoked in its own process.
+	*/
 	void _desktop_run(void);
 
 #ifdef __cplusplus
